8a78145464a827d725405ca4ea4d15621e9d3b1e
HarryMerckel <7945337+HarryMerckel@users.noreply.github.com>
2021-11-14 23:11:31 +0000

    Add columns

--- user_interface.py
+++ user_interface.py
@@ -5,6 +5,8 @@ import datetime
 
 MASTER_PASSCODE = "69420"
 
+PRINTER_COLS = 2
+
 
 def main():
     sg.theme("Dark Blue 12")
@@ -17,7 +19,8 @@ def main():
 
     backend = Backend()
 
-    printer_layout = []
+    printer_layout = [[]]
+    col_counter = 0
     for printer in backend.fleet.printer_access.keys():
         frame_layout = [[sg.T("Status:", size=(10, 1), justification="right"),
                          sg.T("Loading...", key=f"{printer} status", size=(38, 1))
@@ -31,21 +34,30 @@ def main():
                         [sg.T("Progress:", size=(10, 1), justification="right"),
                          sg.T("Loading...", key=f"{printer} progress", size=(38, 1))
                          ],
-                        [sg.B("Start Print", key=f"{printer} start", disabled=True, size=(16, 1)),
-                         sg.B("Finish Print", key=f"{printer} finish", disabled=True, size=(16, 1)),
-                         sg.B("Settings", key=f"{printer} settings", disabled=True, size=(16, 1))
-                         ]
+                        # [sg.B("Start Print", key=f"{printer} start", disabled=True, size=(16, 1)),
+                        #  sg.B("Finish Print", key=f"{printer} finish", disabled=True, size=(16, 1)),
+                        #  sg.B("Settings", key=f"{printer} settings", disabled=True, size=(16, 1))
+                        #  ],
                         ]
-        printer_layout.append([sg.Frame(printer, frame_layout, expand_x=True, key=f"{printer} frame")])
+        if col_counter < PRINTER_COLS:
+            col_counter += 1
+            printer_layout[-1].append(sg.Frame(printer, frame_layout, key=f"{printer} frame"))
+        else:
+            printer_layout.append([sg.Frame(printer, frame_layout, key=f"{printer} frame")])
+            col_counter = 1
 
-    layout = [[sg.Text('iForge 3D Print Automation System', justification='center', font=("Helvetica", 16))],
-              [sg.B('Exit')],
+    layout = [[sg.Text('iForge 3D Print Automation System', justification='center', font=("Helvetica", 16), expand_x=True)],
               [sg.Frame("Printers", key="printer_frame", layout=printer_layout, expand_x=True)],
+              [sg.B("Start Print", key=f"start", disabled=True, size=(16, 1)),
+               sg.B("Finish Print", key=f"finish", disabled=True, size=(16, 1)),
+               sg.B("Settings", key=f"settings", disabled=True, size=(16, 1)),
+               sg.B('Exit', size=(16, 1)),
+               ],
               ]
 
     temp_window.close()
 
-    window = sg.Window('iForge Printer Control', layout)
+    window = sg.Window('iForge Printer Control', layout, element_justification="center")
     window.finalize()
 
     # This is an Event Loop
