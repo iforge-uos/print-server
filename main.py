344dd4edbf51bd6b8a2205d87c7c214506dd0563
domRG <22130302+domRG@users.noreply.github.com>
2022-06-23 17:23:43 +0100

    (Dis)Connect options

--- main.py
+++ main.py
@@ -33,6 +33,7 @@ def get_number_in_list(elem_list):
 
 
 def list_printers(backend):
+    backend.update()
     print(f"Printers:")
     for printer in backend.fleet.printers.keys():
         print(f"{printer:20s} - {backend.fleet.printers[printer]['details']['state']}")
@@ -185,6 +186,46 @@ def cancel_print(backend):
     print(f"Go and check {printing_printers[n]} is clear and ready to print again.")
     time.sleep(10)
 
+def detach_printer(backend):
+    online_printers = []
+    for i_printer in backend.printers.keys():
+        if "available" in backend.printers[i_printer]["details"]["state"]:
+            online_printers.append(i_printer)
+
+    if len(online_printers) == 0:
+        print("No available printers to detach, try again later")
+        return
+
+    print("Select printer to detach:")
+
+    n = get_number_in_list(online_printers)
+    if n == -1:
+        return
+
+    print(f'Attempting to detach {online_printers[n]}, please wait')
+    backend.disconnect_printer(online_printers[n])
+    print(f'{online_printers[n]} is now {backend.printers[online_printers[n]]["details"]["state"].lower()}')
+
+def attach_printer(backend):
+    offline_printers = []
+    for i_printer in backend.printers.keys():
+        if "offline" in backend.printers[i_printer]["details"]["state"]:
+            offline_printers.append(i_printer)
+
+    if len(offline_printers) == 0:
+        print("No offline printers to attach, try again later")
+        return
+
+    print("Select printer to attach:")
+
+    n = get_number_in_list(offline_printers)
+    if n == -1:
+        return
+
+    print(f'Attempting to attach {offline_printers[n]}, please wait')
+    backend.connect_printer(offline_printers[n])
+    print(f'{offline_printers[n]} is now {backend.printers[offline_printers[n]]["details"]["state"].lower()}')
+
 
 if __name__ == '__main__':
     # parser = argparse.ArgumentParser(description='iForge 3D Print Queue Management System')
@@ -213,7 +254,7 @@ if __name__ == '__main__':
                        "'f'\t-\tFinish print handling (Complete/Fail)\n" \
                        "'c'\t-\tCancel print\n" \
                        "'r'\t-\tRefresh printers (slow)\n" \
-                       "'a'\t-\tAdmin Mode"
+                       # "'a'\t-\tAdmin Mode"
 
     admin_option_list = "\nAdmin Options:\n" \
                         "'c'\t-\tConnect a printer\n" \
@@ -244,7 +285,16 @@ if __name__ == '__main__':
 
         elif choice == "A":  # attempt to enter admin mode
             pwd = getpass.getpass("Enter Admin Password:\n>> ")
-            print(base_option_list + admin_option_list)
+            if pwd == "":
+                backend.connect()
+
+                print(admin_option_list)
+                choice = input(">> ").upper()
+
+                if choice == "C":
+                    attach_printer(backend)
+                elif choice == "D":
+                    detach_printer(backend)
 
         elif choice in ["Q"]:
             exit()
