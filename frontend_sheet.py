cdb36c850c01732ea2acb4488dd9be14ee4c8a51
Ewan <117749476+ejcooper@users.noreply.github.com>
2023-04-29 13:33:45 +0100

    Update frontend_sheet.py

--- frontend_sheet.py
+++ frontend_sheet.py
@@ -45,7 +45,7 @@ def print_joblist(joblist):
     print_list([f"{job[2].split(',')[-1][1:-2][:32]:32s}"
                 f"\t{time.strftime('%H:%M:%S', time.gmtime(job[3] * 24 * 60 * 60)):8s}"
                 f"\t{job[7][:8]:8s}"
-                f"\t{job[13][:20]}" 
+                f"   {job[13][:20]}" 
                 for job in joblist.loc[:].values.tolist()[:14]])
 
 
