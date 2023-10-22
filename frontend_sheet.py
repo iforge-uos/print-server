636fb83696d6a3b564fb9d6cf55bfcdc55555c0a
Sam Piper <sampii@protonmail.com>
2023-10-22 15:43:15 +0100

    Update frontend_sheet.py
    keep exotics secret

--- frontend_sheet.py
+++ frontend_sheet.py
@@ -256,7 +256,7 @@ def run():
                               options=[
                                   inputs.Option("mainspace", "m"),
                                   inputs.Option("heartspace", "h"),
-                                  inputs.Option("exotic", "e")       #secret=True)
+                                  inputs.Option("exotic", "e", secret=True)
                               ])
 
     backend = Backend(printer_group=str(group).capitalize())
